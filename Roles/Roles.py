dc8bfd10295b8bdfd997b763a917c5bf23007226
HPK-MrJJ <150632808+HPK-MrJJ@users.noreply.github.com>
2024-05-22 18:17:06 -0400

    Update Roles.py

--- Roles/Roles.py
+++ Roles/Roles.py
@@ -1,4 +1,5 @@
 from redbot.core import commands
+from discord.ext import commands
 import discord
 
 def is_owner_overridable():
@@ -14,12 +15,12 @@ class Roles(commands.Cog):
         self.bot = bot
 
     @is_owner_overridable()    
-    @commands.command(name="add_role")
-    async def add_role(self,ctx,role):
+    @bot.command()
+    async def add_role(self,ctx,role,username):
         await ctx.send(f"adding role {role} to user {ctx.message.author} now!")
         try:
             var = discord.utils.get(ctx.guild.roles, name = role)
         except Exception as e:
             await ctx.send(f"{e}\n Please give a valid role name.") 
-        member = ctx.message.author # Member object that you want to add the role to
+        member = guild.get_member_named(username) # Member object that you want to add the role to
         await member.add_roles(var) # Adds the role to the member
