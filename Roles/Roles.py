a4f44eea8cf2e8ff46a3302fb5f404909b8c7d9d
HPK-MrJJ <150632808+HPK-MrJJ@users.noreply.github.com>
2024-05-22 17:52:57 -0400

    Add Roles

---
+++ Roles/Roles.py
@@ -0,0 +1,16 @@
+from redbot.core import commands
+import discord
+
+class Roles(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    async def add_role(self,ctx,role):
+        await ctx.send(f"adding role {role} to user {ctx.message.author} now!")
+        try:
+            var = discord.utils.get(ctx.guild.roles, name = role)
+        except Exception as e:
+            await ctx.send(f"{e}\n Please give a valid role name.") 
+        member = ctx.message.author # Member object that you want to add the role to
+        await member.add_roles(var) # Adds the role to the member
---
+++ Roles/__init__.py
@@ -0,0 +1,5 @@
+from Roles import Roles
+
+
+async def setup(bot):
+    await bot.add_cog(Roles(bot))
