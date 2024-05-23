cb7821fd195fa065db7f284bbb94c3d86a52b74c
HPK-MrJJ <150632808+HPK-MrJJ@users.noreply.github.com>
2024-05-22 20:21:54 -0400

    Update Roles.py

--- Roles/Roles.py
+++ Roles/Roles.py
@@ -38,3 +38,16 @@ class Roles(red_commands.Cog):
             await ctx.send("You can not give members that role.")
             return
         await ctx.send(f"Role {role} has been added to {username}.")
+        
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        first_char = message.content[0]
+        if not first_char.isalpha():
+            return
+
+        # Check if the message contains the trigger phrase
+        if 'fuck you zalora-bot' in message.content.lower():
+            # Respond to the message
+            await message.reply('Well fuck you too!')
+        if 'best wishes to Montrandec' in message.content.lower() or 'best wishes to r9238yfh' in message.content.lower() or f'best wishes to {ctx.guild.get_member_named(r9238yfh)}' in message.content.lower():
+            await message.reply('Amen, may he soon return to the Fund with a trouble-free mind.')
